3a98fbefaceeb17ee721cbbb22a91bd0c08ee636
DaeWonPark <101173462+DaeWon9@users.noreply.github.com>
2023-02-25 21:21:11 +0900

    [Silver IV] Title: 로프, Time: 128 ms, Memory: 39104 KB -BaekjoonHub

---
+++ 백준/Silver/2217. 로프/로프.py
@@ -0,0 +1,16 @@
+import sys
+
+N = int(input())
+
+weight_list = []
+max_weight_list = []
+
+for _ in range(N):
+    weight_list.append(int(sys.stdin.readline().rstrip()))
+
+weight_list = sorted(weight_list, reverse=True)
+
+for index in range(N):
+    max_weight_list.append(weight_list[index] * (index + 1))
+
+print(max(max_weight_list))
