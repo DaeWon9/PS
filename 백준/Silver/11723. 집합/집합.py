54d501a11dfb391430952d6364fcc55d2bd100fb
DaeWonPark <101173462+DaeWon9@users.noreply.github.com>
2023-02-26 13:26:22 +0900

    [Silver V] Title: 집합, Time: 3792 ms, Memory: 31256 KB -BaekjoonHub

---
+++ 백준/Silver/11723. 집합/집합.py
@@ -0,0 +1,31 @@
+import sys
+
+M = int(input())
+S = set()
+
+for i in range(M):
+    inputData = sys.stdin.readline().rstrip()
+
+    if inputData == 'all':
+        S = set([1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20])
+    elif inputData == 'empty':
+        S = set()
+    else:
+        function, value = inputData.split(" ")
+        value = int(value)
+        if function == 'add':
+            if value not in S:
+                S.add(value)
+        elif function == 'remove':
+            if value in S:
+                S.remove(value)
+        elif function == 'check':
+            if value in S:
+                print(1)
+            else:
+                print(0)
+        elif function == 'toggle':
+            if value in S:
+                S.remove(value)
+            else:
+                S.add(value)
