1c7595d6f39f91cbd711475e28677f688141c32a
DaeWonPark <101173462+DaeWon9@users.noreply.github.com>
2023-02-28 20:34:13 +0900

    [Silver V] Title: 다리 놓기, Time: 40 ms, Memory: 33376 KB -BaekjoonHub

--- 백준/Silver/1010. 다리 놓기/다리 놓기.py
+++ 백준/Silver/1010. 다리 놓기/다리 놓기.py
@@ -1,10 +1,12 @@
 import math
+import sys
 
-T = int(input())
+T = int(sys.stdin.readline().rstrip())
 
 for _ in range(T):
-    R, N = map(int, input().split(" "))
+    R, N = map(int, sys.stdin.readline().rstrip().split(" "))
     if (N - R < N and N != R):
         R = N - R
 
     print(int(math.factorial(N) / (math.factorial(N-R) * math.factorial(R))))
+    
