5ea978e2baf5e1409012245f207ae0dbe708f241
DaeWonPark <101173462+DaeWon9@users.noreply.github.com>
2023-03-03 14:25:10 +0900

    [Silver III] Title: 2×n 타일링, Time: 72 ms, Memory: 31256 KB -BaekjoonHub

---
+++ 백준/Silver/11726. 2×n 타일링/2×n 타일링.py
@@ -0,0 +1,11 @@
+n = int(input())
+
+count_dict = dict()
+
+count_dict[1] = 1
+count_dict[2] = 2
+
+for index in range(3, n+1):
+    count_dict[index] = count_dict[index-1] + count_dict[index-2]
+
+print(count_dict[n] % 10007)
