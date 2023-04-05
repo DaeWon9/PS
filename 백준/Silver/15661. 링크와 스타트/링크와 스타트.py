9483ea13676414b467a0dfe8e1ca6e882250645b
DaeWonPark <101173462+DaeWon9@users.noreply.github.com>
2023-04-05 13:50:06 +0900

    [Silver I] Title: 링크와 스타트, Time: 7436 ms, Memory: 57932 KB -BaekjoonHub

---
+++ 백준/Silver/15661. 링크와 스타트/링크와 스타트.py
@@ -0,0 +1,27 @@
+import sys
+from itertools import combinations
+
+N = int(sys.stdin.readline())
+whole_member = set([ i for i in range(N)])
+array = []
+min_value = 20*100
+
+def get_score(teams, array):
+    sum = 0
+    for i in teams:
+        for j in teams:
+            sum = sum + array[i][j]
+
+    return sum
+
+for row in range(N):
+    array.append(list(map(int, sys.stdin.readline().rstrip().split())))
+
+for i in range(2, N//2+1): #스타트 팀원의 수를 i로 관리 최대 N-1
+    for combi in list(combinations(range(N), i)):
+        start_team = combi
+        link_team = whole_member - set(combi)
+
+        min_value = min(min_value, abs(get_score(start_team, array) - get_score(link_team, array)))
+
+print(min_value)
