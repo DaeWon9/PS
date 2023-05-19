c0626fd7e76ba8fba6efbc769ec324aaaef92d4c
DaeWonPark <101173462+DaeWon9@users.noreply.github.com>
2023-05-19 11:27:27 +0900

    [level 2] Title: 두 큐 합 같게 만들기, Time: 53.94 ms, Memory: 19.3 MB -BaekjoonHub

--- 프로그래머스/lv2/118667. 두 큐 합 같게 만들기/두 큐 합 같게 만들기.py
+++ 프로그래머스/lv2/118667. 두 큐 합 같게 만들기/두 큐 합 같게 만들기.py
@@ -1,6 +1,38 @@
-def solution(queue1, queue2):
-    answer = -1
-    avg = (sum(queue1) + sum(queue2)) / 2
+from collections import deque
 
-    w
+def solution(queue1, queue2):
+    answer = 0
+    queue1_sum = sum(queue1)
+    queue2_sum = sum(queue2)
+    
+    deque1 = deque(queue1)
+    deque2 = deque(queue2)
+    
+    max_count = (len(queue1) + len(queue2)) * 2
+    
+    if (queue1_sum + queue2_sum) % 2 == 1: # 홀수면 아예 불가능
+        return -1
+    
+    while (queue1_sum != queue2_sum):
+        if (answer > max_count):
+            return -1
+        
+        if deque1 and queue1_sum > queue2_sum: #큰쪽에서 pop
+            pop_value = deque1.popleft()
+            deque2.append(pop_value)
+            queue1_sum -= pop_value
+            queue2_sum += pop_value
+            answer += 1
+            continue
+            
+        if deque2 and queue1_sum < queue2_sum: #큰쪽에서 pop
+            pop_value = deque2.popleft()
+            deque1.append(pop_value)
+            queue2_sum -= pop_value
+            queue1_sum += pop_value
+            answer += 1
+            continue
+            
+        answer += 1
+    
     return answer
