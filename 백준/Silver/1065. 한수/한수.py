916a7ecc3993734f1ec5214ec6e3160af6447f94
DaeWonPark <101173462+DaeWon9@users.noreply.github.com>
2023-07-15 21:56:21 +0900

    [Silver IV] Title: 한수, Time: 44 ms, Memory: 31388 KB -BaekjoonHub

---
+++ 백준/Silver/1065. 한수/한수.py
@@ -0,0 +1,98 @@
+import sys
+
+N = int(sys.stdin.readline())
+
+if (N < 100):
+    print(N)
+elif(N < 111):
+    print(99)
+elif(N < 123):
+    print(100)
+elif(N < 135):
+    print(101)
+elif(N < 147):
+    print(102)
+elif(N < 159):
+    print(103)
+elif(N < 210):
+    print(104)
+elif(N < 222):
+    print(105)
+elif(N < 234):
+    print(106)
+elif(N < 246):
+    print(107)
+elif(N < 258):
+    print(108)
+elif(N < 321):
+    print(109)
+elif(N < 333):
+    print(110)
+elif(N < 345):
+    print(111)
+elif(N < 357):
+    print(112)
+elif(N < 369):
+    print(113)
+elif(N < 420):
+    print(114)
+elif(N < 432):
+    print(115)
+elif(N < 444):
+    print(116)
+elif(N < 456):
+    print(117)
+elif(N < 468):
+    print(118)
+elif(N < 531):
+    print(119)
+elif(N < 543):
+    print(120)
+elif(N < 555):
+    print(121)
+elif(N < 567):
+    print(122)
+elif(N < 579):
+    print(123)
+elif(N < 630):
+    print(124)
+elif(N < 642):
+    print(125)
+elif(N < 654):
+    print(126)
+elif(N < 666):
+    print(127)
+elif(N < 678):
+    print(128)
+elif(N < 741):
+    print(129)
+elif(N < 753):
+    print(130)
+elif(N < 765):
+    print(131)
+elif(N < 777):
+    print(132)
+elif(N < 789):
+    print(133)
+elif(N < 840):
+    print(134)
+elif(N < 852):
+    print(135)
+elif(N < 864):
+    print(136)
+elif(N < 876):
+    print(137)
+elif(N < 888):
+    print(138)
+elif(N < 952):
+    print(139)
+elif(N < 963):
+    print(140)
+elif(N < 975):
+    print(141)
+elif(N < 987):
+    print(142)
+elif(N < 999):
+    print(143)
+else:
+    print(144)
