0569999cfb57d3fd0ced9c01b740fef27d109a92
DaeWonPark <101173462+DaeWon9@users.noreply.github.com>
2023-07-19 22:13:57 +0900

    [Silver II] Title: 스택 수열, Time: 132 ms, Memory: 35240 KB -BaekjoonHub

--- 백준/Silver/1874. 스택 수열/스택 수열.py
+++ 백준/Silver/1874. 스택 수열/스택 수열.py
@@ -1,28 +1,23 @@
 import sys
+from collections import deque
 
 n = int(sys.stdin.readline())
-stack = []
+stack = deque()
 i = 0
-result = []
+result = deque()
 
 for _ in range(n):
     x = int(sys.stdin.readline())
-    while True:
-        if x in stack:
-            if stack[-1] == x:
-                stack.pop()
-                result.append('-')
-                break
-            else:
-                print("NO")
-                sys.exit()
-        else:
-            if i+1 > x:
-                print("NO")
-                sys.exit()
-            else:
-                i += 1
-                stack.append(i)
-                result.append('+')
+    while i < x:
+        i += 1
+        stack.append(i)
+        result.append('+')
+
+    if stack[-1] == x:
+        stack.pop()
+        result.append('-')
+    else:
+        print("NO")
+        sys.exit()
 
 print('\n'.join(result))
