fe4ac85362f4a0cfd406c2c9d7dc7fb4e4547f36
DaeWonPark <101173462+DaeWon9@users.noreply.github.com>
2023-08-21 21:09:30 +0900

    [Gold III] Title: 아기 상어, Time: 116 ms, Memory: 34376 KB -BaekjoonHub

---
+++ 백준/Gold/16236. 아기 상어/아기 상어.py
@@ -0,0 +1,82 @@
+import sys
+from collections import deque
+
+
+def find_the_nearest_eatable_fish():
+    visited = [[False for _ in range(N)] for _ in range(N)]
+    visited[baby_shark[0]][baby_shark[1]] = True  # current baby shark pos
+
+    eatable_fish_list = []
+
+    queue = deque()
+    queue.append((baby_shark[0], baby_shark[1], 0))
+
+    while queue:
+        row, col, distance = queue.popleft()
+
+        if distance > 0 and 0 < board[row][col] < baby_shark[2]:  # the nearest eatable
+            eatable_fish_list.append((row, col, distance))
+        else:
+            for index in range(4):
+                dr = row + direction_y[index]
+                dc = col + direction_x[index]
+
+                if (
+                    0 <= dr < N
+                    and 0 <= dc < N
+                    and not visited[dr][dc]
+                    and board[dr][dc] <= baby_shark[2]
+                ):
+                    visited[dr][dc] = True
+                    queue.append((dr, dc, distance + 1))
+
+    if eatable_fish_list:
+        eatable_fish_list.sort(key=lambda x: (x[2], x[0], x[1]))
+        row = eatable_fish_list[0][0]
+        col = eatable_fish_list[0][1]
+
+        board[row][col] = 0
+        baby_shark[0] = row
+        baby_shark[1] = col
+        baby_shark[3] += 1
+
+        if baby_shark[2] == baby_shark[3]:
+            baby_shark[2] += 1
+            baby_shark[3] = 0
+
+        return eatable_fish_list[0][2]
+    else:
+        return -1
+
+
+direction_x = [0, -1, 1, 0]  # 상 좌 우 하
+direction_y = [-1, 0, 0, 1]
+
+N = int(sys.stdin.readline())
+
+baby_shark = []  # row, col, size, stack
+board = []
+time = 0
+
+is_find_baby_shark = False
+
+for row in range(N):
+    input_list = list(map(int, sys.stdin.readline().split()))
+    if not is_find_baby_shark:
+        for col in range(N):
+            if input_list[col] == 9:
+                is_find_baby_shark = True
+                baby_shark = [row, col, 2, 0]
+                input_list[col] = 0
+
+    board.append(input_list)
+
+
+while True:
+    distance = find_the_nearest_eatable_fish()
+    if distance == -1:
+        break
+
+    time += distance
+
+print(time)
