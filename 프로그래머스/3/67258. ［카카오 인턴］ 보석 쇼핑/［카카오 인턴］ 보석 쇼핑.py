87f98b900dde9f4cfa86e80a90aae313510c2690
DaeWonPark <101173462+DaeWon9@users.noreply.github.com>
2023-11-22 00:21:41 +0900

    [level 3] Title: [카카오 인턴] 보석 쇼핑, Time: 86.53 ms, Memory: 21.4 MB -BaekjoonHub

---
+++ 프로그래머스/3/67258. ［카카오 인턴］ 보석 쇼핑/［카카오 인턴］ 보석 쇼핑.py
@@ -0,0 +1,85 @@
+from collections import deque, defaultdict
+
+def solution(gems):
+    gem_dict = defaultdict(int)
+    answer = []
+    result_list = []
+    
+    gem_set = set(gems)
+    #print(gem_set)
+    
+    gem_name_list = deque()
+    
+    dq = deque()
+    count = 0
+    pop_data = 0
+    pop_name = ''
+    # print(len(gem_set))
+    
+    for i in range(len(gems)):
+        # print("-----")
+        # print("dq : ", dq)
+        # print("count : ", count)
+        if (count < len(gem_set)):  
+            if (gem_dict[gems[i]] < 1):
+                count += 1
+            gem_dict[gems[i]] += 1
+            dq.append([i, gems[i]])
+        else:
+            pop_data, pop_name = dq.popleft()
+            gem_dict[pop_name] -= 1
+            result_list.append([pop_data + 1, len(dq) + pop_data + 1])
+            
+            if ((len(dq) + pop_data + 1) - (pop_data + 1) + 1 == len(gem_set)):
+                break
+                
+            while(True):
+                # print("---in while--")
+                # print("dq : ", dq)
+                # print("count : ", count)
+                # print("pop_data : ", pop_data)
+                # print("pop_name : ", pop_name)
+                if(len(dq) < len(gem_set)):
+                    break
+                if (gem_dict[pop_name] < 1):
+                    count -= 1
+                    break
+                pop_data, pop_name = dq.popleft()
+                gem_dict[pop_name] -= 1
+            
+            result_list.append([pop_data + 1, len(dq) + pop_data + 1])
+            
+            if (gem_dict[gems[i]] < 1):
+                count += 1
+                
+            gem_dict[gems[i]] += 1
+            dq.append([i, gems[i]])
+        
+        
+    if (pop_name == ''):
+        pop_name = dq[0][1]
+
+    if (count == len(gem_set)):
+        while(len(dq) >= len(gem_set)):
+            # print("---out while--")
+            # print("dq : ", dq)
+            # print("count : ", count)
+            # print("pop_data : ", pop_data)
+            # print("pop_name : ", pop_name)
+
+            if (gem_dict[pop_name] < 1):
+                result_list.append([pop_data + 1, len(dq) + pop_data + 1])
+                break
+
+            pop_data, pop_name = dq.popleft()
+            gem_dict[pop_name] -= 1
+            
+        # print("add : ", [pop_data + 1, len(dq) + pop_data + 1])
+        result_list.append([pop_data + 1, len(dq) + pop_data + 1])
+    
+    
+        
+    
+    result_list = sorted(result_list, key = lambda x : ((x[1] - x[0]), x[0]))
+    answer = result_list[0]
+    return answer
