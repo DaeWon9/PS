5c4bde3f0cc435811a9db376614c2218ce477b82
DaeWonPark <101173462+DaeWon9@users.noreply.github.com>
2024-01-16 11:58:53 +0900

    [Silver I] Title: 효율적인 해킹, Time: 14972 ms, Memory: 210664 KB -BaekjoonHub

--- 백준/Silver/1325. 효율적인 해킹/효율적인 해킹.py
+++ 백준/Silver/1325. 효율적인 해킹/효율적인 해킹.py
@@ -1,40 +1,42 @@
 import sys
-from collections import deque, defaultdict
+from collections import defaultdict, deque
 
-def bfs(graph, start, visited):
-    global result
+n, m = map(int, sys.stdin.readline().split())
+computer_dict = defaultdict(list)
+connected_count_list = []
 
-    queue = deque([start])
-    visited[start] = True
+def get_connected_computer_count(root_computer):
+    visited = [False for _ in range(n + 1)]
+    queue = deque()
+    queue.append(root_computer)
+
+    count = 0
 
     while queue:
-        result += 1
-        v = queue.popleft()
-        for i in graph[v]:
-            if not visited[i]:
-                queue.append(i)
-                visited[i] = True
+        temp_computer = queue.popleft()
 
-computer_count, edge_count = map(int, sys.stdin.readline().rstrip().split())
-graph = defaultdict(list)
-result_list = []
-max_count = 0
+        for computer in computer_dict[temp_computer]:
+            if (not visited[computer]):
+                visited[computer] = True
+                queue.append(computer)
+                count += 1
+                
+        visited[temp_computer] = True
 
-for _ in range(edge_count):
-    a, b = map(int, sys.stdin.readline().rstrip().split())
-    graph[b] += [a]
+    return count
 
-for i in range(1, computer_count + 1):
-    visited = [False] * (computer_count + 1)
-    result = 0
-    bfs(graph, i, visited)
-    result_list.append([i, result])
-    max_count = max(max_count, result)
+for _ in range(m):
+    a_computer, b_computer = map(int, sys.stdin.readline().split()) # a 가 b 를 신뢰
+    computer_dict[b_computer].append(a_computer)
 
-result_list.sort(key = lambda x :(x[1]), reverse=True)
+for key in range(1, n + 1):
+    connected_count_list.append((key, get_connected_computer_count(key)))
 
-for data in result_list:
-    if (data[1] == max_count):
-        print(data[0], end=' ')
-    else:
+connected_count_list.sort(key = lambda x : (-x[1], x[0]))
+max_count = connected_count_list[0][1]
+
+for count_info in connected_count_list:
+    if (count_info[1] != max_count):
         break
+
+    print(count_info[0], end=' ')
