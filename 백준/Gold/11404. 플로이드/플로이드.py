f3c0a71afe1fb92c32595c2385aed9c0d8181df2
DaeWonPark <101173462+DaeWon9@users.noreply.github.com>
2024-01-28 16:32:19 +0900

    [Gold IV] Title: 플로이드, Time: 612 ms, Memory: 31120 KB -BaekjoonHub

---
+++ 백준/Gold/11404. 플로이드/플로이드.py
@@ -0,0 +1,28 @@
+import sys
+input = sys.stdin.readline
+
+n = int(input())
+m = int(input())
+
+graph = [[2147483647 for _ in range(n + 1)] for _ in range(n + 1)]
+
+for _ in range(m):
+    v1, v2, cost = map(int, input().split())
+    if (graph[v1][v2] > cost):
+        graph[v1][v2] = cost
+
+for i in range(1, n + 1):
+    graph[i][i] = 0
+
+for k in range(1, n + 1):
+    for i in range(1, n + 1):
+        for j in range(1, n + 1):
+            graph[i][j] = min(graph[i][j], graph[i][k] + graph[k][j])
+
+for i in range(1, n + 1):
+    for j in range(1, n + 1):
+        if (graph[i][j] == 2147483647):
+            print(0, end=' ')
+        else:
+            print(graph[i][j], end=' ')
+    print()
