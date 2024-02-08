f93b6742c7a436a1f9f53e0f6a92850d83bb2579
DaeWonPark <101173462+DaeWon9@users.noreply.github.com>
2024-02-08 20:53:38 +0900

    [Gold II] Title: 중앙값 구하기, Time: 60 ms, Memory: 33316 KB -BaekjoonHub

--- 백준/Gold/2696. 중앙값 구하기/중앙값 구하기.py
+++ 백준/Gold/2696. 중앙값 구하기/중앙값 구하기.py
@@ -1,4 +1,5 @@
 import sys
+from bisect import insort_left
 input = sys.stdin.readline
 
 def solution(num_list):
@@ -6,34 +7,7 @@ def solution(num_list):
     answer = []
 
     for index, num in enumerate(num_list):
-        if (index == 0):
-            sorted_list.append(num)
-            answer.append(num)
-            continue
-        
-        left_index = 0
-        right_index = index - 1
-        target_index = 0
- 
-        while left_index <= right_index:
-            mid_index = (left_index + right_index) // 2
-
-            if (sorted_list[mid_index] < num):
-                left_index = mid_index + 1
-            else:
-                right_index = mid_index - 1
-            
-            if (left_index == mid_index or mid_index == right_index):
-                if (sorted_list[mid_index] < num):
-                    target_index = mid_index + 1
-                else:
-                    target_index = mid_index
-                break
-
-        if (target_index == -1):
-            sorted_list.insert(0, num)
-        else:
-            sorted_list.insert(target_index, num)
+        insort_left(sorted_list, num)
 
         if ((index + 1) % 2 == 1):
             answer.append(sorted_list[index // 2])
