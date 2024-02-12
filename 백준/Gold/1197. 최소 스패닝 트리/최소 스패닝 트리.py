58c98a09e7b5e1902dc9059b07dc0d48b56cfca9
DaeWonPark <101173462+DaeWon9@users.noreply.github.com>
2024-02-12 20:01:50 +0900

    [Gold IV] Title: 최소 스패닝 트리, Time: 452 ms, Memory: 57800 KB -BaekjoonHub

---
+++ 백준/Gold/1197. 최소 스패닝 트리/최소 스패닝 트리.py
@@ -0,0 +1,37 @@
+import sys
+from collections import defaultdict
+input = sys.stdin.readline
+
+def find(x):
+    if root[x] == x:
+        return x
+    
+    root[x] = find(root[x])
+    return root[x]
+
+def union(x, y):
+    x = find(x)
+    y = find(y)
+    root[y] = x
+
+V, E = map(int, input().split())
+
+edges = []
+for _ in range(E):
+    edges.append(list(map(int, input().split())))
+
+root = defaultdict(int)
+for i in range(1, V+1):
+    root[i] = i
+
+edges = sorted(edges, key=lambda x: x[2])
+weight = 0
+
+for edge in edges:
+    if find(edge[0]) == find(edge[1]):
+        continue
+
+    weight += edge[2]
+    union(edge[0], edge[1])
+
+print(weight)
