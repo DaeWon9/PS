b92fc652ff2a0c9799eca2d045fa3e7487f43122
DaeWonPark <101173462+DaeWon9@users.noreply.github.com>
2024-03-17 00:15:36 +0900

    [Gold II] Title: 플로이드 2, Time: 436 ms, Memory: 34096 KB -BaekjoonHub

---
+++ 백준/Gold/11780. 플로이드 2/플로이드 2.py
@@ -0,0 +1,38 @@
+import sys
+from collections import deque
+input = sys.stdin.readline
+
+n = int(input())
+m = int(input())
+
+graph = [[2147483647 for _ in range(n + 1)] for _ in range(n + 1)]
+for i in range(1, n + 1):
+    graph[i][i] = 0
+    
+path = [[[r] for _ in range(n + 1)] for r in range(n + 1)]
+
+for _ in range(m):
+    v1, v2, cost = map(int, input().split())
+    if (graph[v1][v2] > cost):
+        graph[v1][v2] = cost
+
+for k in range(1, n + 1):
+    for i in range(1, n + 1):
+        for j in range(1, n + 1):
+            if (graph[i][j] > graph[i][k] + graph[k][j]):
+                path[i][j] = path[i][k] + path[k][j]
+                graph[i][j] = graph[i][k] + graph[k][j]
+
+for r in range(1, n + 1):
+    for c in range(1, n + 1):
+        if (graph[r][c] == 2147483647):
+            print(0)
+        else:
+            print(graph[r][c])
+    
+for r in range(1, n + 1):
+    for c in range(1, n + 1):
+        if (r == c or graph[r][c] == 2147483647):
+            print(0)
+        else:
+            print(len(path[r][c]) + 1, *path[r][c] , c)
