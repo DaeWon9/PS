c988d33a2a6bef299ad53af8fcacfd55a3285bd2
DaeWonPark <101173462+DaeWon9@users.noreply.github.com>
2024-03-16 19:56:28 +0900

    [Gold III] Title: 백양로 브레이크, Time: 3332 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Gold/11562. 백양로 브레이크/백양로 브레이크.py
+++ 백준/Gold/11562. 백양로 브레이크/백양로 브레이크.py
@@ -3,7 +3,6 @@ input = sys.stdin.readline
 
 n, m = map(int, input().split())
 graph = [[2147483647 for _ in range(n + 1)] for _ in range(n + 1)]
-questions = []
 
 for _ in range(m):
     v1, v2, case = map(int, input().split())
@@ -13,17 +12,16 @@ for _ in range(m):
     else:
         graph[v2][v1] = 1
 
-k = int(input())
-for _ in range(k):
-    questions.append(tuple(map(int, input().split())))
+for k in range(1, n + 1):
+    for i in range(1, n + 1):
+        for j in range(1, n + 1):
+            if (graph[i][j] > graph[i][k] + graph[k][j]):
+                graph[i][j] = graph[i][k] + graph[k][j]
 
 for i in range(1, n + 1):
     graph[i][i] = 0
 
-for k in range(1, n + 1):
-    for i in range(1, n + 1):
-        for j in range(1, n + 1):
-            graph[i][j] = min(graph[i][j], graph[i][k] + graph[k][j])
-
-for start, end in questions:
+k = int(input())
+for _ in range(k):
+    start, end = map(int, input().split())
     print(graph[start][end])
