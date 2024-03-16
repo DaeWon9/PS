1af382bcab72306a504c7c9f707cbf4d87cd6ce6
DaeWonPark <101173462+DaeWon9@users.noreply.github.com>
2024-03-16 22:07:31 +0900

    [Gold III] Title: 택배, Time: 300 ms, Memory: 111964 KB -BaekjoonHub

--- 백준/Gold/1719. 택배/택배.py
+++ 백준/Gold/1719. 택배/택배.py
@@ -1,13 +1,6 @@
 import sys
 input = sys.stdin.readline
 
-def find(r, c):
-    while True:
-        if (prev_path[r][c] == prev_path[r][prev_path[r][c]]):
-            return prev_path[r][c]
-        else:
-            prev_path[r][c] = prev_path[r][prev_path[r][c]]
-
 n, m = map(int, input().split())
 graph = [[2147483647 for _ in range(n + 1)] for _ in range(n + 1)]
 prev_path = [['-' for _ in range(n + 1)] for _ in range(n + 1)]
@@ -26,13 +19,8 @@ for k in range(1, n + 1):
     for i in range(1, n + 1):
         for j in range(1, n + 1):
             if (graph[i][j] > graph[i][k] + graph[k][j]):
-                prev_path[i][j] = k
+                prev_path[i][j] = prev_path[i][k]
                 graph[i][j] = graph[i][k] + graph[k][j]
-                
-for r in range(1, n + 1):
-    for c in range(1, n + 1):
-        if (prev_path[r][c] == '-'):
-            print('-', end = ' ')
-        else:
-            print(find(r, c), end = ' ')
-    print()
+
+for r in prev_path[1:]:
+    print(*r[1:])
