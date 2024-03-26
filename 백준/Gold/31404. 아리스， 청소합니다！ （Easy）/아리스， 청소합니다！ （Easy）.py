f8c91c4ffe060a2cb125c8b8ae19acb9be14d6c0
DaeWonPark <101173462+DaeWon9@users.noreply.github.com>
2024-03-26 18:38:22 +0900

    [Gold III] Title: 아리스, 청소합니다! (Easy), Time: 196 ms, Memory: 114916 KB -BaekjoonHub

--- 백준/Gold/31404. 아리스， 청소합니다！ （Easy）/아리스， 청소합니다！ （Easy）.py
+++ 백준/Gold/31404. 아리스， 청소합니다！ （Easy）/아리스， 청소합니다！ （Easy）.py
@@ -2,11 +2,28 @@ import sys
 from collections import deque
 input = sys.stdin.readline
 
-def calaculate_index(r, c):
-    return r * W + c
-    
-direction_x = [0, 1, 0, -1] # 상 우 하 좌 (시계방향)
+direction_x = [0, 1, 0, -1]
 direction_y = [-1, 0, 1, 0]
+
+def calc_index(r, c, d):
+    return (r * W + c) * 4 + d
+
+def find(x):
+    if (parent[x] == x):
+        return x
+    
+    parent[x] = find(parent[x])
+    return parent[x]
+
+def union(x, y):
+    x = find(x)
+    y = find(y)
+
+    if (x == y):
+        return
+    
+    parent[y] = x
+    rank[x] += rank[y]
 
 def is_movable(dr, dc):
     if (0 <= dr < H and 0 <= dc < W):
@@ -14,58 +31,68 @@ def is_movable(dr, dc):
     return False
 
 def solution():
-    global answer, b_count, parent
+    global current_r, current_c, current_d
+    answer = 0
+    b_count = 0
 
-    while queue:
-        current_r, current_c, current_d = queue.pop()
-        is_rule_b = False
-        new_direction = 0
+    while True:
+        if (not is_movable(current_r, current_c)):
+            return answer
 
         if (clear_board[current_r][current_c]): # RULE_B
-            b_count += 1
-            is_rule_b = True
-            new_direction = (current_d + rule_b[current_r][current_c]) % 4
+            start_index = calc_index(current_r, current_c, current_d)
 
-            if (path_start_index == -1):
-                next_index_set.clear()
-                path_start_index = calaculate_index(current_r, current_c)
+            storage = deque([start_index])
+            is_first_visit = True
 
-        else: # RULE_A
+            while (is_movable(current_r, current_c) and clear_board[current_r][current_c]):
+                current_index = calc_index(current_r, current_c, current_d)
+
+                if (not is_first_visit and current_index == start_index):
+                    return answer
+                
+                root = find(current_index)
+
+                if (root == current_index):
+                    b_count += 1
+                    current_d = (current_d + rule_b[current_r][current_c]) % 4
+                    current_r += direction_y[current_d]
+                    current_c += direction_x[current_d]
+                    storage.append(calc_index(current_r, current_c, current_d))
+                else:
+                    current_r = root // (4 * W)
+                    current_c = root // 4 % W
+                    current_d = root % 4
+                    b_count += rank[root] - 1
+                    storage.append(root)
+
+                is_first_visit = False
+
+            if (not is_movable(current_r, current_c)):
+                return answer
+            
+            while True:
+                x = storage.pop()
+                y = storage.pop()
+                union(x, y)
+                if (not storage):
+                    break
+                storage.append(y)
+        else:
+            clear_board[current_r][current_c] = True
+            current_d = (current_d + rule_a[current_r][current_c]) % 4
+            current_r += direction_y[current_d]
+            current_c += direction_x[current_d]
             answer += (b_count + 1)
             b_count = 0
-            clear_board[current_r][current_c] = True
-            new_direction = (current_d + rule_a[current_r][current_c]) % 4
-            path_start_index = -1
-
-        dr = current_r + direction_y[new_direction]
-        dc = current_c + direction_x[new_direction]
-
-        if (is_movable(dr, dc)):
-            if (is_rule_b):
-                current_index = calaculate_index(current_r, current_c)
-                next_index = calaculate_index(dr, dc)
-
-                if (current_index != path_start_index):
-                    queue.append((dr, dc, new_direction))
-                    continue
-                
-                if (next_index in next_index_set):
-                    break
-                
-                next_index_set.add(next_index)
-                
-            queue.append((dr, dc, new_direction))
-        else:
-            break
 
 H, W = map(int, input().split())
-R, C, D = map(int, input().split())
+current_r, current_c, current_d = map(int, input().split())
+
+parent = [i for i in range(H * W * 4)] # 4방향 별
+rank = [1 for _ in range(H * W * 4)]
 
 clear_board = [[False for _ in range(W)] for _ in range(H)]
-
-path_start_index = -1
-next_index_set = set()
-
 rule_a = []
 rule_b = []
 
@@ -75,11 +102,4 @@ for _ in range(H):
 for _ in range(H):
     rule_b.append(list(map(int, str(input().rstrip()))))
 
-queue = deque()
-queue.append((R, C, D))
-answer = 0
-b_count = 0
-
-solution()
-
-print(answer)
+print(solution())
