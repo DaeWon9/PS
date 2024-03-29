635a0f22f9cbc8d3f28de6e545e20e5b081e4cd6
DaeWonPark <101173462+DaeWon9@users.noreply.github.com>
2024-03-29 17:36:01 +0900

    [Silver III] Title: N과 M (1), Time: 208 ms, Memory: 31120 KB -BaekjoonHub

---
+++ 백준/Silver/15649. N과 M （1）/N과 M （1）.py
@@ -0,0 +1,19 @@
+import sys
+input = sys.stdin.readline
+
+def solution(combi:list, pivot):
+    if (len(combi) == M):
+        print(*combi)
+        return
+    
+    for i in range(1, N + 1):
+        if (num_list[i] in combi):
+            continue
+        combi.append(num_list[i])
+        solution(combi, i + 1)
+        combi.pop()
+
+N, M = map(int, input().split())
+num_list = [i for i in range(N + 1)]
+
+solution([], 1)
