333ed0739b9b042b2fe54763048986b669c59e86
DaeWonPark <101173462+DaeWon9@users.noreply.github.com>
2024-06-25 00:30:39 +0900

    [Bronze III] Title: K 2K 게임, Time: 104 ms, Memory: 33860 KB, Score: 100 point -BaekjoonHub

--- 백준/Bronze/21603. K 2K 게임/K 2K 게임.py
+++ 백준/Bronze/21603. K 2K 게임/K 2K 게임.py
@@ -2,12 +2,14 @@ import sys
 input = sys.stdin.readline
 
 N, K = map(int, input().split())
+K2 = K * 2
+K2 = K2 % 10
 K = K % 10
 
 answer = []
 
 for i in range(1, N + 1):
-    if (i % 10 == K or i % 10 == 2 * K):
+    if (i % 10 == K or i % 10 == K2):
         continue
     answer.append(i)
 
