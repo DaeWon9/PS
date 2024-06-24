4d7fffb3fe1fff6d2cb61b192bd3e7687ace5f34
DaeWonPark <101173462+DaeWon9@users.noreply.github.com>
2024-06-24 23:42:19 +0900

    [Silver II] Title: 동전 게임, Time: 60 ms, Memory: 34088 KB -BaekjoonHub

--- 백준/Silver/9079. 동전 게임/동전 게임.py
+++ 백준/Silver/9079. 동전 게임/동전 게임.py
@@ -1,119 +1,47 @@
-from collections import deque
 import sys
+from collections import deque
 
-input = sys.stdin.readline
+t = int(sys.stdin.readline())
+reverse = [[0,1,2],[3,4,5],[6,7,8],[0,3,6],[1,4,7],[2,5,8],[0,4,8],[2,4,6]]
 
-# 다 같은 면인지 확인
-def check_same(board):
-    temp = board[0][0]
-    for i in range(3):
-        for j in range(3):
-            if board[i][j] != temp:
-                return False
-    return True
+for _ in range(t) :
+    coin = []
+    for i in range(3) :
+        line = sys.stdin.readline().split()
+        for l in line :
+            if l == 'H' :
+                coin.append(False)
+            else :
+                coin.append(True)
+    queue = deque()
+    queue.append((coin,0))
+    visit = set()
+    visit.add(str(coin))
 
-# 중복 확인을 위해 2진수의 10진수값 계산
-def calculate(board):
-    score = 0
-    for i in range(3):
-        for j in range(3):
-            score = score * 2 + board[i][j]
-    return score
+    p = 0
 
-# 값을 다시 그래프로 변환
-def score_to_board(score):
-    for i in range(2, -1, -1):
-        for j in range(2, -1, -1):
-            board[i][j] = score % 2
-            score = score // 2
+    while queue :
+        tmp,cnt = queue.popleft()
 
-    return board
+        a = 0
+        for i in range(8) :
+            if tmp[i]!=tmp[i+1] :
+                a = 1
+        if a==0 :
+            print(cnt)
+            p = 1
+            break
+        
 
-# bfs
-def find_arr(board):
-    global ans
-    q = deque()
-    score_arr = []
+        for re in reverse :
+            x = [True for _ in range(9)]
+            for i in range(9) :
+                x[i] = tmp[i]
+            for r in re :
+                x[r] = not tmp[r]
+            if str(x) not in visit :
+                queue.append((x,cnt+1))
+                visit.add(str(x))
 
-    score = calculate(board)
-
-    q.append((score, 0))
-
-    while q:
-        # next_board, count = q.popleft()
-        score, count = q.popleft()
-        # print(score_arr)
-
-        next_board = score_to_board(score)
-
-        if check_same(next_board):
-            ans = count
-            return
-
-        # 행 변환 (3가지)
-        for i in range(3):
-            for j in range(3):
-                next_board[i][j] = 0 if next_board[i][j] == 1 else 1
-                
-            # print(next_board)
-            next_score = calculate(next_board)
-            if next_score not in score_arr:
-                score_arr.append(next_score)
-                q.append((next_score, count+1))
-
-            for j in range(3):
-                next_board[i][j] = 0 if next_board[i][j] == 1 else 1
-
-        # 열 변환 (3가지)
-        for i in range(3):
-            for j in range(3):
-                next_board[j][i] = 0 if next_board[j][i] == 1 else 1
-            
-            next_score = calculate(next_board)
-            if next_score not in score_arr:
-                score_arr.append(next_score)
-                q.append((next_score, count+1))
-
-            for j in range(3):
-                next_board[j][i] = 0 if next_board[j][i] == 1 else 1
-            
-        # 대각선 변환 (2가지)
-        for i in range(3):
-            next_board[i][i] = 0 if next_board[i][i] == 1 else 1
-
-        next_score = calculate(next_board)
-        if next_score not in score_arr:
-            score_arr.append(next_score)
-            q.append((next_score, count+1))
-
-        for i in range(3):
-            next_board[i][i] = 0 if next_board[i][i] == 1 else 1
-
-        for i in range(3):
-            next_board[2-i][i] = 0 if next_board[2-i][i] == 1 else 1
-
-        next_score = calculate(next_board)
-        if next_score not in score_arr:
-            score_arr.append(next_score)
-            q.append((next_score, count+1))
-
-        for i in range(3):
-            next_board[2-i][i] = 0 if next_board[2-i][i] == 1 else 1
-
-    ans = -1
-
-T = int(input())
-for _ in range(T):
-    board = []
-    for i in range(3):
-        temp = list(input().split())
-        for j in range(3):
-            if temp[j] == 'T':
-                temp[j] = 1
-            else:
-                temp[j] = 0
-        board.append(temp)
-    ans = 0
-
-    find_arr(board)
-    print(ans)
+    if p==0 :
+        print(-1)
