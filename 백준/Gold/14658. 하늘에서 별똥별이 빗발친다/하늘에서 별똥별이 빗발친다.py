4323dd639105d450744941a24109f80cba8c3b84
DaeWonPark <101173462+DaeWon9@users.noreply.github.com>
2024-10-30 17:08:48 +0900

    [Gold III] Title: 하늘에서 별똥별이 빗발친다, Time: 32 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Gold/14658. 하늘에서 별똥별이 빗발친다/하늘에서 별똥별이 빗발친다.py
+++ 백준/Gold/14658. 하늘에서 별똥별이 빗발친다/하늘에서 별똥별이 빗발친다.py
@@ -2,20 +2,30 @@ import sys
 input = sys.stdin.readline
 
 N, M, L, K = map(int, input().split())
+answer= 0
 stars = []
 for _ in range(K):
     x, y = map(int, input().split())
     stars.append([x, y])
 
-answer = 0
-for i in range(K):
-    for j in range(K):
-        count = 0
-        for x, y in stars:
-            if (stars[i][1] <= y <= stars[i][1] + L and stars[j][0] <= x <= stars[j][0] + L):
-                count += 1
+stars.sort()
+
+for x, y in stars: # x 기준
+    x1 = x
+    x2 = x + L
+
+    target_stars_y = sorted([y for x, y in stars if (x1 <= x <= x2)])
+
+    left = 0
+
+    for right in range(len(target_stars_y)):
         
-        if (answer < count):
-            answer = count
+        while True:
+            if (target_stars_y[right] - target_stars_y[left] <= L):
+                break
+            left += 1
+
+        if (answer < right - left + 1):
+            answer = right - left + 1
 
 print(K - answer)
