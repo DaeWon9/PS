b0ebdb4559f17cf722fb39a19ad77ad1c34728e5
DaeWonPark <101173462+DaeWon9@users.noreply.github.com>
2024-11-28 18:51:33 +0900

    [Platinum IV] Title: 도미노, Time: 596 ms, Memory: 221852 KB -BaekjoonHub

--- 백준/Platinum/4196. 도미노/도미노.py
+++ 백준/Platinum/4196. 도미노/도미노.py
@@ -1,5 +1,6 @@
 import sys
 from collections import defaultdict, deque
+import heapq
 input = sys.stdin.readline
 
 T = int(input())
@@ -14,16 +15,16 @@ for _ in range(T):
 
     queue = deque()
     answer = 0
+    max_heap = []
 
     for _ in range(M):
-        x, y = map(int, input().split()) # x번 블록이 넘어지면 y번 블록도 넘어짐
-
+        x, y = map(int, input().split())  # x번 블록이 넘어지면 y번 블록도 넘어짐
         indegree[y] += 1
         outdegree[x] += 1
         adj_vertices[x].append(y)
 
-    for idx in range(1, N+1):
-        if (indegree[idx] == 0):
+    for idx in range(1, N + 1):
+        if indegree[idx] == 0:
             queue.append(idx)
             answer += 1
             remain_domino.remove(idx)
@@ -32,21 +33,22 @@ for _ in range(T):
         v = queue.popleft()
 
         for adj_vertex in adj_vertices[v]:
-            if (visited[adj_vertex]):
+            if visited[adj_vertex]:
                 continue
-            
+
             queue.append(adj_vertex)
-            if (adj_vertex in remain_domino):
+            if adj_vertex in remain_domino:
                 remain_domino.remove(adj_vertex)
                 visited[adj_vertex] = True
 
+    for domino in remain_domino:
+        heapq.heappush(max_heap, (-outdegree[domino], domino))
+
     while remain_domino:
-        max_outdegree = 0
-        target_domino = 0
-        for key in remain_domino:
-            if (outdegree[key] > 0 and max_outdegree < outdegree[key]):
-                target_domino = key
-                max_outdegree = outdegree[key]
+        while max_heap:
+            _, target_domino = heapq.heappop(max_heap)
+            if (target_domino in remain_domino):
+                break
 
         remain_domino.remove(target_domino)
         queue.append(target_domino)
@@ -57,9 +59,9 @@ for _ in range(T):
             v = queue.popleft()
 
             for adj_vertex in adj_vertices[v]:
-                if (visited[adj_vertex]):
+                if visited[adj_vertex]:
                     continue
-                
+
                 queue.append(adj_vertex)
                 if (adj_vertex in remain_domino):
                     remain_domino.remove(adj_vertex)
