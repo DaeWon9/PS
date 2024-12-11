93b43a56a2fb92736cd5e2ce14c71dc513c0e23b
DaeWonPark <101173462+DaeWon9@users.noreply.github.com>
2024-12-11 16:31:21 +0900

    [Gold IV] Title: 우체국, Time: 228 ms, Memory: 46504 KB -BaekjoonHub

--- 백준/Gold/2285. 우체국/우체국.py
+++ 백준/Gold/2285. 우체국/우체국.py
@@ -1,21 +1,25 @@
 import sys
 
+villiage_info_list = []
+whole_people_count = 0
+
 n = int(sys.stdin.readline())
-total_people_count = 0
-villages = []
 
 for _ in range(n):
-    pos, people_count = sys.stdin.readline().split()
-    total_people_count += int(people_count)
-    villages.append((int(pos), int(people_count)))
+    n_viliage, people = map(int, sys.stdin.readline().split())
+    villiage_info_list.append((n_viliage, people))
+    whole_people_count += people
 
-villages.sort(key=lambda x: x[0])
+villiage_info_list.sort() 
+half_people_count = whole_people_count / 2
+value = 0
+answer = 0
 
-half_people_count = total_people_count // 2
-
-people_count = 0
-for village in villages:
-    people_count += village[1]
-    if people_count > half_people_count:
-        print(village[0])
+for village_info in villiage_info_list:
+    value += village_info[1]
+    
+    if (value >= half_people_count):
+        answer = village_info[0]
         break
+
+print(answer)
