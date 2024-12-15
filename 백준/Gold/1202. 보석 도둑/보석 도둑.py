a1c52b336c1e33a27cafc80402aa3e2c17c7bc23
DaeWonPark <101173462+DaeWon9@users.noreply.github.com>
2024-12-15 16:03:46 +0900

    [Gold II] Title: 보석 도둑, Time: 1072 ms, Memory: 169132 KB -BaekjoonHub

---
+++ 백준/Gold/1202. 보석 도둑/보석 도둑.py
@@ -0,0 +1,38 @@
+import sys
+from bisect import bisect_left
+input = sys.stdin.readline
+
+def find(x):
+    if (x == parent[x]):
+        return x
+    
+    parent[x] = find(parent[x])
+    return parent[x]
+
+N, K = map(int, input().split())
+C = []
+jewel_list = []
+parent = [i for i in range(K+1)]
+
+for _ in range(N):
+    W, V = map(int, input().split())
+    jewel_list.append((W, V))
+
+for _ in range(K):
+    C.append(int(input()))
+
+C.sort()
+jewel_list.sort(key= lambda x: (-x[1], x[0]))
+answer = 0
+
+for jewel_w, jewel_v in jewel_list:
+    idx = bisect_left(C, jewel_w)
+    available_idx = find(idx)
+
+    if (available_idx >= K):
+        continue
+
+    answer += jewel_v
+    parent[available_idx] += 1
+
+print(answer)
