31c4fe7e54f813adb77a25991275165149a6c358
DaeWonPark <101173462+DaeWon9@users.noreply.github.com>
2025-01-11 16:04:27 +0900

    [Gold IV] Title: 관악산 등산, Time: 212 ms, Memory: 117528 KB -BaekjoonHub

--- 백준/Gold/14699. 관악산 등산/관악산 등산.py
+++ 백준/Gold/14699. 관악산 등산/관악산 등산.py
@@ -8,10 +8,12 @@ height_list = [(input_heights[i], i+1) for i in range(N)]
 
 height_dict = defaultdict(int)
 adj_vertices = defaultdict(list)
+queue = deque()
 dp = [1] * (N)
 
 for height, id in height_list:
     height_dict[id] = height
+    queue.append((id, 1))
 
 for _ in range(M):
     v1, v2 = map(int, input().split())
@@ -24,16 +26,17 @@ for _ in range(M):
     elif (v1_height > v2_height):
         adj_vertices[v1].append(v2)
 
-for id in range(1, N+1):
-    queue = deque([(id, 1)])
+while queue:
+    v, c = queue.popleft()
 
-    while queue:
-        v, c = queue.popleft()
+    if (dp[v-1] > c):
+        continue
 
-        for adj_vertex in adj_vertices[v]:
-            if (dp[adj_vertex-1] < c+1):
-                dp[adj_vertex-1] = c+1
-                queue.append((adj_vertex, c+1))
+    for adj_vertex in adj_vertices[v]:
+        if (dp[adj_vertex-1] < c+1):
+            dp[adj_vertex-1] = c+1
+
+            queue.appendleft((adj_vertex, c+1))
 
 for ans in dp:
     print(ans)
