f4d40feefdf9ec64c089141100431163605a4693
DaeWonPark <101173462+DaeWon9@users.noreply.github.com>
2025-01-11 16:00:58 +0900

    [Gold IV] Title: 관악산 등산, Time: 1760 ms, Memory: 39664 KB -BaekjoonHub

---
+++ 백준/Gold/14699. 관악산 등산/관악산 등산.py
@@ -0,0 +1,39 @@
+import sys
+from collections import defaultdict, deque
+input = sys.stdin.readline
+
+N, M = map(int, input().split())
+input_heights = list(map(int, input().split()))
+height_list = [(input_heights[i], i+1) for i in range(N)]
+
+height_dict = defaultdict(int)
+adj_vertices = defaultdict(list)
+dp = [1] * (N)
+
+for height, id in height_list:
+    height_dict[id] = height
+
+for _ in range(M):
+    v1, v2 = map(int, input().split())
+
+    v1_height = height_dict[v1]
+    v2_height = height_dict[v2]
+
+    if (v1_height < v2_height):
+        adj_vertices[v2].append(v1)
+    elif (v1_height > v2_height):
+        adj_vertices[v1].append(v2)
+
+for id in range(1, N+1):
+    queue = deque([(id, 1)])
+
+    while queue:
+        v, c = queue.popleft()
+
+        for adj_vertex in adj_vertices[v]:
+            if (dp[adj_vertex-1] < c+1):
+                dp[adj_vertex-1] = c+1
+                queue.append((adj_vertex, c+1))
+
+for ans in dp:
+    print(ans)
