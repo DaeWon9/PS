1b8115dc73a47c9aab05a9af37216c2748989f33
DaeWonPark <101173462+DaeWon9@users.noreply.github.com>
2025-01-13 15:36:34 +0900

    [Gold IV] Title: 가르침, Time: 192 ms, Memory: 110736 KB -BaekjoonHub

---
+++ 백준/Gold/1062. 가르침/가르침.py
@@ -0,0 +1,45 @@
+import sys
+from itertools import combinations
+input = sys.stdin.readline
+
+default_char = {'a', 'n', 't', 'i', 'c'}
+
+N, K = map(int, input().split())
+char_to_bit = {chr(i): 1 << (i - ord('a')) for i in range(ord('a'), ord('z') + 1)}
+word_bit_list = []
+all_char_bit = 0
+
+for _ in range(N):
+    word = input().rstrip()
+    word_bit = 0
+
+    for char in set(word) - default_char:
+        word_bit |= char_to_bit[char]
+
+    word_bit_list.append(word_bit)
+
+    for char in set(word) - default_char:
+        all_char_bit |= char_to_bit[char]
+
+if (K < 5):
+    print(0)
+    exit()
+
+base_bit = sum(char_to_bit[char] for char in default_char)
+all_chars = [char for char in range(26) if (1 << char) & all_char_bit]
+max_chars = min(len(all_chars), K - 5)
+answer = 0
+
+for combi in combinations(all_chars, max_chars):
+    combi_bit = base_bit
+    for char in combi:
+        combi_bit |= (1 << char)
+
+    temp_answer = 0
+    for word_bit in word_bit_list:
+        if word_bit & combi_bit == word_bit:
+            temp_answer += 1
+
+    answer = max(answer, temp_answer)
+
+print(answer)
