349e4ab6c550cce883482835625a43c470967dcf
DaeWonPark <101173462+DaeWon9@users.noreply.github.com>
2025-01-13 15:51:48 +0900

    [Gold IV] Title: 가르침, Time: 188 ms, Memory: 110736 KB -BaekjoonHub

--- 백준/Gold/1062. 가르침/가르침.py
+++ 백준/Gold/1062. 가르침/가르침.py
@@ -15,11 +15,9 @@ for _ in range(N):
 
     for char in set(word) - default_char:
         word_bit |= char_to_bit[char]
+        all_char_bit |= char_to_bit[char]
 
     word_bit_list.append(word_bit)
-
-    for char in set(word) - default_char:
-        all_char_bit |= char_to_bit[char]
 
 if (K < 5):
     print(0)
@@ -27,19 +25,19 @@ if (K < 5):
 
 base_bit = sum(char_to_bit[char] for char in default_char)
 all_chars = [char for char in range(26) if (1 << char) & all_char_bit]
-max_chars = min(len(all_chars), K - 5)
 answer = 0
 
-for combi in combinations(all_chars, max_chars):
+for combi in combinations(all_chars, min(len(all_chars), K - 5)):
     combi_bit = base_bit
     for char in combi:
         combi_bit |= (1 << char)
 
     temp_answer = 0
     for word_bit in word_bit_list:
-        if word_bit & combi_bit == word_bit:
+        if (word_bit & combi_bit == word_bit):
             temp_answer += 1
 
-    answer = max(answer, temp_answer)
+    if (answer < temp_answer):
+        answer = temp_answer
 
 print(answer)
