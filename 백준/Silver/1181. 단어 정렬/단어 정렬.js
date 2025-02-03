092c865b9372898a02578a1a5b15a4207a1fb532
DaeWonPark <101173462+DaeWon9@users.noreply.github.com>
2025-02-03 13:40:16 +0900

    [Silver V] Title: 단어 정렬, Time: 960 ms, Memory: 19636 KB -BaekjoonHub

---
+++ 백준/Silver/1181. 단어 정렬/단어 정렬.js
@@ -0,0 +1,22 @@
+const fs = require("fs");
+const filePath = process.platform === "linux" ? "/dev/stdin" : "input.txt";
+const input = fs.readFileSync(filePath).toString().trim().split("\n");
+
+const N = Number(input[0]);
+let idx = 1;
+let words = [];
+
+for (let i = 0; i < N; i++) {
+    words.push(input[idx++]);
+}
+
+let uniqueWords = [...new Set(words)];
+
+uniqueWords.sort((a, b) => {
+    if (a.length === b.length) return a.localeCompare(b);
+    return a.length - b.length;
+});
+
+for (const word of uniqueWords) {
+    console.log(word);
+}
