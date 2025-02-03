58f8e4f5c8d2fdb3036a9f4e773f0fdeb2d71fee
DaeWonPark <101173462+DaeWon9@users.noreply.github.com>
2025-02-03 13:24:29 +0900

    [Silver I] Title: 볼 모으기, Time: 176 ms, Memory: 11400 KB, Score: 100 point -BaekjoonHub

--- 백준/Silver/17615. 볼 모으기/볼 모으기.js
+++ 백준/Silver/17615. 볼 모으기/볼 모으기.js
@@ -5,53 +5,76 @@ const input = fs.readFileSync(filePath).toString().trim().split("\n");
 const N = Number(input[0]);
 const inputData = input[1];
 
-const lastChar = inputData[inputData.length - 1];
-const firstCahr = inputData[0];
-const RGroup = inputData.split("B").filter((item) => item);
-const BGroup = inputData.split("R").filter((item) => item);
-
-if (RGroup.length === 0 || BGroup.length === 0 || (RGroup.length === 1 && BGroup.length === 1)) {
-    console.log(0);
-    process.exit(0);
-}
+const firstChar = inputData[0];
+const lastChar = inputData[N - 1];
+let firstGroupFlag = false;
 
 let RCenterCount = 0;
 let BCenterCount = 0;
-let answer = 2147483647;
+let firstGroupCount = 1;
+let lastGroupCount = 0;
+let prevChar = firstChar;
+let tempCount = 0;
 
-for (const data of RGroup.slice(1, RGroup.length - 1)) {
-    RCenterCount += data.length;
+for (let i = 1; i < N; i++) {
+    if (!firstGroupFlag) {
+        if (inputData[i] === firstChar) {
+            firstGroupCount++;
+        } else {
+            firstGroupFlag = true;
+            prevChar = inputData[i];
+            tempCount = 1;
+            if (inputData[i] === "R") RCenterCount++;
+            else BCenterCount++;
+        }
+        continue;
+    }
+
+    if (inputData[i] === "R") RCenterCount++;
+    else BCenterCount++;
+
+    if (prevChar === inputData[i]) {
+        tempCount++;
+    } else {
+        prevChar = inputData[i];
+        tempCount = 1;
+    }
 }
+lastGroupCount = tempCount;
 
-for (const data of BGroup.slice(1, BGroup.length - 1)) {
-    BCenterCount += data.length;
-}
-
-if (firstCahr === "R") {
-    answer = Math.min(
-        answer,
-        RCenterCount + RGroup[RGroup.length - 1].length,
-        BCenterCount + BGroup[0].length + BGroup[BGroup.length - 1].length
-    );
+if (lastChar === "R") {
+    RCenterCount -= lastGroupCount;
 } else {
-    answer = Math.min(
-        answer,
-        BCenterCount + BGroup[BGroup.length - 1].length,
-        RCenterCount + RGroup[0].length + RGroup[RGroup.length - 1].length
-    );
+    BCenterCount -= lastGroupCount;
 }
+
+let answer = 2147483647;
 
 if (lastChar === "R") {
     answer = Math.min(
         answer,
-        RCenterCount + RGroup[0].length,
-        BCenterCount + BGroup[0].length + BGroup[BGroup.length - 1].length
+        RCenterCount + (firstChar === "R" ? firstGroupCount : 0),
+        BCenterCount + (firstChar === "B" ? firstGroupCount : 0) + (lastChar === "B" ? lastGroupCount : 0)
     );
 } else {
     answer = Math.min(
         answer,
-        BCenterCount + BGroup[0].length,
-        RCenterCount + RGroup[0].length + RGroup[RGroup.length - 1].length
+        BCenterCount + (firstChar === "B" ? firstGroupCount : 0),
+        RCenterCount + (firstChar === "R" ? firstGroupCount : 0) + (lastChar === "R" ? lastGroupCount : 0)
+    );
+}
+
+if (firstChar === "R") {
+    answer = Math.min(
+        answer,
+        RCenterCount + (lastChar === "R" ? lastGroupCount : 0),
+        BCenterCount + (firstChar === "B" ? firstGroupCount : 0) + (lastChar === "B" ? lastGroupCount : 0)
+    );
+} else {
+    answer = Math.min(
+        answer,
+        BCenterCount + (lastChar === "B" ? lastGroupCount : 0),
+        RCenterCount + (firstChar === "R" ? firstGroupCount : 0) + (lastChar === "R" ? lastGroupCount : 0)
     );
 }
 
