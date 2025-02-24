4cdebb43ab7969731245524229cb017857945cc4
DaeWonPark <101173462+DaeWon9@users.noreply.github.com>
2025-02-24 16:28:56 +0900

    [Silver III] Title: 1로 만들기, Time: 36 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Silver/1463. 1로 만들기/1로 만들기.py
+++ 백준/Silver/1463. 1로 만들기/1로 만들기.py
@@ -1,22 +1,23 @@
+import sys
+input = sys.stdin.readline
+
 X = int(input())
-
-leaf = [X]
-
+leaf = { X }
 count = 0
+
 if (X == 1):
     pass
 else:
     while(1 not in leaf):
-        parent = leaf
-        children = []
-        for node in parent:
+        children = set()
+        for node in leaf:
             if (node % 3 == 0):
-                children.append(node / 3)
+                children.add(node // 3)
             if (node % 2 == 0):
-                children.append(node / 2)
-            children.append(node - 1)
+                children.add(node // 2)
+            children.add(node - 1)
         
-        leaf = list(set(children))
+        leaf = children
         count = count + 1
 
 print(count)
