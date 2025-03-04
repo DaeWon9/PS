f3bde8e854cb544f3c378b388ff73eff36eaf2a0
DaeWonPark <101173462+DaeWon9@users.noreply.github.com>
2025-03-04 22:41:49 +0900

    [level 2] Title: 비밀 코드 해독, Time: 226.46 ms, Memory: 26.5 MB -BaekjoonHub

--- 프로그래머스/2/388352. 비밀 코드 해독/비밀 코드 해독.py
+++ 프로그래머스/2/388352. 비밀 코드 해독/비밀 코드 해독.py
@@ -1,3 +1,40 @@
+def combinations(arr, n, start = 0, path = [], result = []):
+    if (len(path) == n):
+        result.append(path[:])
+        return
+    
+    for i in range(start, len(arr)):
+        path.append(arr[i])
+        combinations(arr, n, i+1, path, result)
+        path.pop()
+        
+    return result
+
 def solution(n, q, ans):
     answer = 0
+    all_set = set(range(1, n+1))
+    arr = sorted(zip(ans, q))
+    
+    for ans, q in arr:
+        if (ans > 0):
+            break
+        
+        for data in q:
+            try:
+                all_set.remove(data)
+            except:
+                pass
+
+    for combi in combinations(list(all_set), 5):
+        pivot_set = set(combi)
+        flag = True
+        for ans, q in arr:
+            intersection_set = pivot_set.intersection(set(q))
+            if (len(intersection_set) != ans):
+                flag = False
+                break
+                
+        if (flag):
+            answer += 1
+
     return answer
