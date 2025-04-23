aa53435182b677381af6af15373d587447ecc290
DaeWonPark <101173462+DaeWon9@users.noreply.github.com>
2025-04-23 20:10:08 +0900

    [Platinum I] Title: 레드 블루 스패닝 트리 2, Time: 228 ms, Memory: 137152 KB -BaekjoonHub

--- 백준/Platinum/16950. 레드 블루 스패닝 트리 2/레드 블루 스패닝 트리 2.py
+++ 백준/Platinum/16950. 레드 블루 스패닝 트리 2/레드 블루 스패닝 트리 2.py
@@ -12,12 +12,10 @@ def union(x, y):
     y = find(y)
     if (x == y):
         return False
-    if (rank[x] < rank[y]):
-        parent[x] = y
-    else:
+    if (x < y):
         parent[y] = x
-        if (rank[x] == rank[y]):
-            rank[x] += 1
+    else:
+        parent[x] = y
     return True
 
 n, m, k = map(int, input().split())
@@ -34,25 +32,6 @@ for _ in range(m):
         red_edges.append((u, v))
 
 parent = [i for i in range(n+1)]
-rank = [0] * (n+1)
-max_blue = 0
-edges_used = 0
-
-for u, v in blue_edges:
-    if (union(u, v)):
-        max_blue += 1
-        edges_used += 1
-        if (edges_used == n-1):
-            break
-
-for u, v in red_edges:
-    if (edges_used == n-1):
-        break
-    if (union(u, v)):
-        edges_used += 1
-
-parent = [i for i in range(n+1)]
-rank = [0] * (n+1)
 min_blue = 0
 edges_used = 0
 min_blue_edges = set()
@@ -60,8 +39,6 @@ min_blue_edges = set()
 for u, v in red_edges:
     if (union(u, v)):
         edges_used += 1
-        if (edges_used == n-1):
-            break
 
 for u, v in blue_edges:
     if (edges_used == n-1):
@@ -71,12 +48,8 @@ for u, v in blue_edges:
         min_blue += 1
         edges_used += 1
 
-if (not (min_blue <= k <= max_blue)):
-    print(0)
-    sys.exit()
 
 parent = [i for i in range(n+1)]
-rank = [0] * (n+1)
 result = []
 blue_count = 0
 edges_used = 0
@@ -86,29 +59,23 @@ for u, v in min_blue_edges:
         result.append((u, v))
         blue_count += 1
         edges_used += 1
-        if (edges_used == n-1):
-            break
 
-if (blue_count < k):
-    for u, v in blue_edges:
-        if (blue_count == k):
-            break
-        if ((u, v) in min_blue_edges):
-            continue
-        if (union(u, v)):
-            result.append((u, v))
-            blue_count += 1
-            edges_used += 1
-            if (edges_used == n-1):
-                break
+for u, v in blue_edges:
+    if (blue_count == k):
+        break
+    if ((u, v) in min_blue_edges):
+        continue
+    if (union(u, v)):
+        result.append((u, v))
+        blue_count += 1
+        edges_used += 1
 
-if (edges_used < n-1):
-    for u, v in red_edges:
-        if (edges_used == n-1):
-            break
-        if (union(u, v)):
-            result.append((u, v))
-            edges_used += 1
+for u, v in red_edges:
+    if (edges_used == n-1):
+        break
+    if (union(u, v)):
+        result.append((u, v))
+        edges_used += 1
 
 if (edges_used == n-1 and blue_count == k):
     for u, v in result:
