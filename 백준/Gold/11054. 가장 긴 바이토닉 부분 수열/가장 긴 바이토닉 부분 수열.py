359c3da9b65988023dd05cc3c448ed2156e26b72
DaeWonPark <101173462+DaeWon9@users.noreply.github.com>
2025-05-02 17:30:40 +0900

    [Gold IV] Title: 가장 긴 바이토닉 부분 수열, Time: 44 ms, Memory: 34476 KB -BaekjoonHub

--- 백준/Gold/11054. 가장 긴 바이토닉 부분 수열/가장 긴 바이토닉 부분 수열.py
+++ 백준/Gold/11054. 가장 긴 바이토닉 부분 수열/가장 긴 바이토닉 부분 수열.py
@@ -6,10 +6,10 @@ N = int(input())
 arr = list(map(int, input().split()))
 reversed_arr = arr[:]
 reversed_arr.reverse()
-increase = [[0, 0, 0] for _ in range(N)]
-decrease = [[0, 0, 0] for _ in range(N)]
-increase[0] = [1, 0, arr[0]]
-decrease[0] = [1, 0, reversed_arr[0]]
+increase = [[0, 0] for _ in range(N)]
+decrease = [[0, 0] for _ in range(N)]
+increase[0] = [1, arr[0]]
+decrease[0] = [1, reversed_arr[0]]
 
 LIS = [arr[0]]
 RLIS = [reversed_arr[0]]
@@ -18,7 +18,7 @@ answer = 0
 for i in range(1, N):
     if (LIS[-1] < arr[i]):
         LIS.append(arr[i])
-        increase[i] = [len(LIS), i, arr[i]]
+        increase[i] = [len(LIS), arr[i]]
     else:
         idx = bisect_left(LIS, arr[i])
         LIS[idx] = arr[i]
@@ -26,7 +26,7 @@ for i in range(1, N):
 
     if (RLIS[-1] < reversed_arr[i]):
         RLIS.append(reversed_arr[i])
-        decrease[i] = [len(RLIS), i, reversed_arr[i]]
+        decrease[i] = [len(RLIS), reversed_arr[i]]
     else:
         idx = bisect_left(RLIS, reversed_arr[i])
         RLIS[idx] = reversed_arr[i]
@@ -37,9 +37,8 @@ for i in range(N):
     decrease_value = decrease[N-i-1]
 
     len_sum = increase_value[0] + decrease_value[0]
-    idx_sum = increase_value[1] + decrease_value[1]
 
-    if (idx_sum == N-1 or increase_value[2] == decrease_value[2]):
+    if (increase_value[1] == decrease_value[1]):
         len_sum -= 1
 
     if (answer < len_sum):
